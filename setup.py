47f0345b58c383a2a5691e5ccfc32b3590f0b106
Thirsty-Robot <Thirsty-Robot@protonmail.com>
2018-07-24 19:49:31 -0500

    Added MANIFEST, modified setup.py and dist

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
 	author='Thirsty-Robot',
 	author_email='Thirsty-Robot@protonmail.com',
 	license='MIT',
-
+	long_description='A library for text-based game development in python. Also, educational',
+	url='https://github.com/Thirsty-Robot/Texy.py-Game-Engine',
 	classifiers=[
 		'Development Status :: 5 - Production/Stable',
 		'Intended Audience :: Developers',
